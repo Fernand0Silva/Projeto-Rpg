64ab7c6b9a74f3bd274937d2bd55cc80415afa99
luiz <luizfernando3995@gmail.com>
2024-08-06 22:22:51 -0300

    second commit

---
+++ Utils/ClaimTypesExtension.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace RpgApi.Utils
+{
+    public static class ClaimTypesExtension
+    {
+        public static int UserId(this ClaimsPrincipal user)// método modificado!!!
+        {
+            var id = user.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier).Value;
+            return int.Parse(id);
+        }
+        
+        
+    }
+}
